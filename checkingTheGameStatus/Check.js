226d4763901b2b59ad4691a785d24c1ea9701332
George-Karpenko <g.r.karpenko@yandex.ru>
2024-01-17 20:32:12 +0300

    findIndex для king

--- checkingTheGameStatus/Check.js
+++ checkingTheGameStatus/Check.js
@@ -6,7 +6,7 @@ export function checkACheck(gridPieces, isAMove, eatenOnAisle) {
   return !!~enemyPieces.findIndex((piece) => {
     return ~piece
       .acceptableMoves({ gridPieces, eatenOnAisle })
-      .findIndex((move) => move.x === king.x && move.y === king.y);
+      .findIndex((move) => move.x === king?.x && move.y === king.y);
   });
   function colorPieces(color) {
     return [].concat(...gridPieces).filter((piece) => piece?.color === color);
