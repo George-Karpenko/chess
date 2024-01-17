24bfec7d282701eaaffc2853f427a72cac5656b7
George-Karpenko <g.r.karpenko@yandex.ru>
2024-01-17 17:30:48 +0300

    начальное значение для pieces в localStorage

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ import ViewModal from "./View/ViewModal.js";
 import ViewPieces from "./View/ViewPieces.js";
 import ViewPromotionChoice from "./View/ViewPromotionChoice.js";
 
-if (!"pieces" in localStorage) {
+if (!("pieces" in localStorage)) {
   localStorage.setItem("pieces", "alpha");
 }
 
